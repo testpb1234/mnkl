440fb0cc69d99a1801ef4c5d148d352cc6a898ab
pilarbuchen <pilarb@wix.com>
2024-04-02 12:45:33 -0500

    mkmlk

--- src/components/new-component/new-component.tsx
+++ src/components/new-component/new-component.tsx
@@ -10,5 +10,5 @@ export interface NewComponentProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const NewComponent = ({ className }: NewComponentProps) => {
-    return <div className={classNames(styles.root, className)}>NewComponent</div>;
+    return <div className={classNames(styles.root, className)}></div>;
 };
